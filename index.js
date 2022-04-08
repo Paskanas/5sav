42e6540eb7fbd564f5bb5231916465b4bd5ef73b
Paskanas <kaspe1992@gmail.com>
2022-04-08 09:40:13 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('-----Uzduotis1------');
+
+console.log('-----Uzduotis2------');
+
+console.log('-----Uzduotis3------');
+
+console.log('-----Uzduotis4------');
+console.log('-----Uzduotis5------');
+console.log('-----Uzduotis6------');
+console.log('-----Uzduotis7------');
+console.log('-----Uzduotis8------');
+console.log('-----Uzduotis9------');
+console.log('-----Uzduotis10------');
+console.log('-----Uzduotis11------');
+console.log('-----Uzduotis12------');
+console.log('-----Uzduotis13------');
